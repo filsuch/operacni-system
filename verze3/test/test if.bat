3f7daff917d91cc39922d7aecb6afc4abff86108
Filip Such <filas.suchomel@gmail.com>
2023-01-20 20:34:56 +0100

    verze 3 dokončena

--- verze3/test/test if.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cls
-set lime=1
-IF "%lime%"=="1" (echo 1 dobře ) ELSE (echo 1 nenastaveno)
-IF "%lime%"=="2" (echo 2 dobře ) else (echo 2 nenastaveno)
-
-echo konec
--- verze3/test/testconfig.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-for /f "tokens=1,2 delims==" %%a in (config.txt) do (
-    if %%a==name set %%a=%%b
-    if %%a==lastName set %%a=%%b
-)
-
- echo %lastName% > out.txt
